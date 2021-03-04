5749d46e4c2cd0d6256c5b7b98f766a675aebe0d
anikod <nevelikobo@gmail.com>
2021-03-04 23:04:16 +0300

    feat: Typical Arrays Problems feature implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
+  if (typeof array === undefined || !array || array.length === 0 ) return 0;
+  //if (array === undefined) return 0;
+  return Math.min(...array) 
+
+
+
   return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === undefined || !array || array.length === 0 ) return 0;
+  return Math.max(...array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array == undefined || !array || array.length === 0 ) return 0;
+  let sum = 0; 
+  array.forEach(function(item) {
+  sum += item;
+  });
+  return sum / array.length; 
 }
